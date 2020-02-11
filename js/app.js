41d6a310471f2abf975c0060185ca6a3574cf58d
myles-richard <myrich22@gmail.com>
2020-02-11 15:44:36 -0800

    some set up for the board

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 console.log('hello there')
+// need to get all my box's and give them variables
+let redBox = document.getElementById('topleft')
+let greenBox = document.getElementById('topright')
+let blueBox = document.getElementById('bottomleft')
+let yellowBox = document.getElementById('bottomright')
+// need to have an empty array to fill with random numbers
+let computerArr = [];
+// need to have an array to match with computer to make sure i did the right combo
+let myArr = [];
